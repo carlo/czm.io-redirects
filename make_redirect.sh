30ccc26eb24022571bed633c57d4130badeb7cd3
Carlo Zottmann <carlo@municode.de>
2016-06-09 18:11:23 +0200

    [CHG] jiffybox ➔ nfs.

--- make_redirect.sh
+++ make_redirect.sh
@@ -5,7 +5,7 @@ set uuid (cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 5 | hea
 set url "https://czm.io/r/$uuid"
 set icon (dirname (status --current-filename))/icon.png
 
-ssh pubcast "echo 'rewrite /$uuid $target;' >> /home/mailcaster/sites/czm.io-redirects.nginx.include";
-  and ssh root@pubcast "service nginx restart" 2>&1 >> /dev/null;
+echo "RewriteRule ^r/$uuid $target [R=301]" >> ~/Google\ Drive/Sites/czm.io/.htaccess;
+  and rsync ~/Google\ Drive/Sites/czm.io/.htaccess nfs-czmio:
   and /usr/local/bin/terminal-notifier -title "Redirect created" -message "$url" -appIcon $icon -open "$url"
 echo $url | pbcopy
